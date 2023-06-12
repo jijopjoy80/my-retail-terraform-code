5a5e0814418438879f62cbe0cc27188fa9eac0df
jijopjoy80 <59017309+jijopjoy80@users.noreply.github.com>
2023-06-12 19:47:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
- # required_version = ">= 0.14.9"
+ required_version = ">= 0.14.9"
 }
 
 provider "aws" {
   profile = "default"
-  region  = "ap-south-1"
+  region  = "us-east-1"
 }
 
 module "vpc" {
@@ -20,7 +20,7 @@ module "vpc" {
   name = "my-retail-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
